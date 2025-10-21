a862cde46a08fdb92af4b17bb9a646a4d4f3a7c9
chichusy <2820027957@qq.com>
2025-10-21 15:42:52 +0800

    second

--- test_env.py
+++ test_env.py
@@ -1,2 +1,16 @@
 import tiktoken
 print("tiktoken imported successfully")
+
+import os
+import time
+import math
+import pickle
+from contextlib import nullcontext
+
+import numpy as np
+import torch
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.distributed import init_process_group, destroy_process_group
+
+from model import GPTConfig, GPT
+print("All imports successful")
